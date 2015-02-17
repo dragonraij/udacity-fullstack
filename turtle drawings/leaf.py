ba140c88c1060941599b506889712e644725af7f
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-02-17 13:24:48 +1300

    Draw plant

--- turtle drawings/leaf.py
+++ turtle drawings/leaf.py
@@ -1,8 +1,10 @@
 import turtle
+import time
 
 turtle.shape("turtle")
+turtle.bgcolor("#4DB8FF")
 turtle.left(90)
-turtle.sety(-250)
+time.sleep(5)
 
 
 def drawGround(dist):
@@ -75,7 +77,37 @@ def drawleaf(dist, angle):
     turtle.back(dist*1.9)
     turtle.right(angle)
     turtle.hideturtle()
+
+def drawFlower(dist):
+    for x in range(0, 25):
+        turtle.begin_fill()
+        turtle.width(3)
+        turtle.color("orange", "pink")
+        turtle.right(15)
+        turtle.forward(dist)
+        turtle.left(45)
+        turtle.forward(dist/2)
+        turtle.left(120)
+        turtle.forward(dist/2)
+        turtle.left(45)
+        turtle.forward(dist)
+        turtle.end_fill()
+    turtle.color("#CCCC00", "#FFFF66")
+    turtle.begin_fill()
+    turtle.penup()
+    turtle.forward(40)
+    turtle.pendown()
     
+    turtle.left(90)
+    turtle.circle(40)
+    turtle.end_fill()
+    #turtle.left(90)
+    #turtle.circle(50,120)
+    #turtle.right(90)
+    #turtle.circle(50,120)
+
+
+turtle.sety(-250)
 drawGround(400)
 turtle.setx(0)
 drawStem(100)    
@@ -86,5 +118,5 @@ drawleaf(30, 50)
 drawStem(50)
 drawleaf(30, -50)
 drawStem(150)    
-
+drawFlower(75)
 turtle.exitonclick()
