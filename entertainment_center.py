0f7d5376d2ab20bfd4842a67aba2a05349a53c5c
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-02-22 23:51:48 +1300

    Space Odyssey

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,8 +1,9 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("toy Story", "A story of a boy and his toys that come to life", "http://www.eggplante.com/wp-content/uploads/2013/09/11-Toy-Story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
-avatar = media.Movie("Avataar", "A marine collides with aliens", "http://www.impawards.com/2009/posters/avatar_ver5_xlg.jpg", "https://www.youtube.com/watch?v=d1_JBMrrYw8")
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "http://www.eggplante.com/wp-content/uploads/2013/09/11-Toy-Story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+avatar = media.Movie("Avatar", "A marine collides with aliens", "http://www.impawards.com/2009/posters/avatar_ver5_xlg.jpg", "https://www.youtube.com/watch?v=d1_JBMrrYw8")
+odyssey = media.Movie("2001: A Space Odyssey", "A trip in space", "http://upload.wikimedia.org/wikipedia/en/e/ef/2001_A_Space_Odyssey_Style_B.jpg","https://www.youtube.com/watch?v=N6ywMnbef6Y")
 #print(toy_story.storyline)     
 #print(avatar.storyline)
 #avatar.show_trailer()
