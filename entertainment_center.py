a36c88589dcea0f02e0ce452ff7b0a832d1cf84b
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-02-22 23:58:41 +1300

    Gunday

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,6 +4,7 @@ import fresh_tomatoes
 toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "http://www.eggplante.com/wp-content/uploads/2013/09/11-Toy-Story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 avatar = media.Movie("Avatar", "A marine collides with aliens", "http://www.impawards.com/2009/posters/avatar_ver5_xlg.jpg", "https://www.youtube.com/watch?v=d1_JBMrrYw8")
 odyssey = media.Movie("2001: A Space Odyssey", "A trip in space", "http://upload.wikimedia.org/wikipedia/en/e/ef/2001_A_Space_Odyssey_Style_B.jpg","https://www.youtube.com/watch?v=N6ywMnbef6Y")
+gudday = media.Movie ("Gunday", "A new Love story", "http://upload.wikimedia.org/wikipedia/en/4/46/Gunday_%282013_film%29.jpg", "https://www.youtube.com/watch?v=lFI09rbuHQ8")
 #print(toy_story.storyline)     
 #print(avatar.storyline)
 #avatar.show_trailer()
